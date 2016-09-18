63db407df66d4767d6e5954c4c9a0acd10bafb5c
Alex Kuiper <alex@nightwhistler.net>
2016-09-18 10:48:17 +0200

    Added IntelliJ shortcuts

--- modules/intellij/manifests/init.pp
+++ modules/intellij/manifests/init.pp
@@ -3,13 +3,21 @@ class intellij {
     $user     = hiera('user')
     $userhome = "/home/${user}"
 
+    $release = '2016.2.4'
+    $build = '162.2032.8'
+
     archive { 'intellij.tar.gz':
-      source          => 'https://download.jetbrains.com/idea/ideaIU-2016.2.4.tar.gz',
+      source          => "https://download.jetbrains.com/idea/ideaIU-${release}.tar.gz",
       path            => "${userhome}/Downloads/intellij.tar.gz",
       extract         => true,
       cleanup         => true,
       extract_path    => "${userhome}/Apps/",
-      creates         => "${userhome}/Apps/idea-IU-162.2032.8",
+      creates         => "${userhome}/Apps/idea-IU-${build}",
+    }
+
+    file { "${userhome}/.local/share/applications/jetbrains-idea.desktop":
+        content => epp('intellij/jetbrains-idea.desktop')
+
     }
 
 }
