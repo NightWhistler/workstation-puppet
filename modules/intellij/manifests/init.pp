65c362095a7c428d99473f83cf529f7099841898
Alex Kuiper <alex@nightwhistler.net>
2016-11-13 12:11:12 +0100

    Updated IntelliJ version.

--- modules/intellij/manifests/init.pp
+++ modules/intellij/manifests/init.pp
@@ -3,8 +3,8 @@ class intellij {
     $user     = hiera('user')
     $userhome = "/home/${user}"
 
-    $release = '2016.2.4'
-    $build = '162.2032.8'
+    $release = '2016.2.5'
+    $build = '162.2228.15'
 
     archive { 'intellij.tar.gz':
       source          => "https://download.jetbrains.com/idea/ideaIU-${release}.tar.gz",
