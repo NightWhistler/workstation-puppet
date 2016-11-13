6d4fa47fd2673084bf76e31ab576e464e37712d3
Alex Kuiper <alex@nightwhistler.net>
2016-11-13 22:50:07 +0100

    Now creates folder if it doesn't exist

--- modules/chromeapp/manifests/init.pp
+++ modules/chromeapp/manifests/init.pp
@@ -5,4 +5,8 @@ class chromeapp {
     $user     = hiera('user')
     $userhome = "/home/${user}"
 
+    file { ["${chromeapp::userhome}/.local/","${chromeapp::userhome}/.local/share/","${chromeapp::userhome}/.local/share/applications/"]:
+        ensure  => 'directory'
+    }
+
 }
