8dcd17227856fd2e8d8f7588e8c171e38e7a16bd
Alex Kuiper <alex@nightwhistler.net>
2016-11-13 22:50:18 +0100

    Added dependency

--- modules/steam/manifests/init.pp
+++ modules/steam/manifests/init.pp
@@ -5,11 +5,16 @@ class steam {
 
   require 'wget'
 
+  package { 'python-apt':
+    ensure  => 'installed'
+  }
+
   wget::fetch { 'steam.deb':
       destination => "${userhome}/Downloads/steam.deb",
       source      => "https://steamcdn-a.akamaihd.net/client/installer/steam.deb",
       timeout     => 0,
       verbose     => false,
+      require     => Package['python-apt']
   }
   
   package { 'steam':
