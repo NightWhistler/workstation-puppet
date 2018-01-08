177180ff8f5c96ee07c7885312426dee1d6bea30
Alex Kuiper <alex@nightwhistler.net>
2018-01-08 16:47:15 +0100

    Fixed signal

--- modules/signal/manifests/init.pp
+++ modules/signal/manifests/init.pp
@@ -1,8 +1,8 @@
 class signal {
 
   apt::key { 'signal':
-    id         => '57F6FB06',
-    key_source => 'https://updates.signal.org/desktop/apt/keys.asc'
+    id         => 'DBA36B5181D0C816F630E889D980A17457F6FB06',
+    source => 'https://updates.signal.org/desktop/apt/keys.asc'
   }
 
   apt::source { 'signal':
