393cbbdb8be69f929ef6ee31facab452762de613
Alex Kuiper <alex@nightwhistler.net>
2018-01-15 20:26:17 +0100

    Added neovim

---
+++ modules/neovim/manifests/init.pp
@@ -0,0 +1,29 @@
+class neovim {
+
+  apt::ppa { 'ppa:neovim-ppa/stable': }
+
+  package { [ 'python-dev', 'python3-dev', 'python3-pip' ]:
+    ensure => 'installed'
+  }
+
+  exec { 'neovim_apt_get_update':
+    command     => 'apt-get update',
+    cwd         => '/tmp',
+    path        => ['/usr/bin'],
+    require     => Apt::Ppa['ppa:neovim-ppa/stable'],
+    subscribe   => Apt::Ppa['ppa:neovim-ppa/stable'],
+    refreshonly => true,
+  }
+    
+  package { 'neovim':
+    ensure  => 'installed',
+    require => [Exec['neovim_apt_get_update'], Package[ 'python-pip', 'python-dev']]
+  }
+
+  #package{ ['websocket-client', 'sexpdata', 'neovim']:
+    #  ensure   => present,
+    #provider => 'pip',
+    #require  => [ Package['python-pip'], ],
+    #}
+
+}
