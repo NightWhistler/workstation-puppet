5280323312d4b3510cda220b7437d2180665c306
Alex Kuiper <alex@nightwhistler.net>
2018-09-15 08:56:30 +0200

    Fixed ordering of modules to fix dependency problems.

--- provision.sh
+++ provision.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 echo "Adding Puppet modules"
+#FIXME: We need to mode to puppet-wget, but for now some modules
+#still depend on the older wget
+
+#sudo /opt/puppetlabs/bin/puppet module install puppet-wget
+sudo /opt/puppetlabs/bin/puppet module install talend-sbt
+sudo /opt/puppetlabs/bin/puppet module install puppet-unattended_upgrades 
+
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs/docker
 sudo /opt/puppetlabs/bin/puppet module install bashtoni-timezone
 sudo /opt/puppetlabs/bin/puppet module install zanloy-ohmyzsh
-sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apt
-sudo /opt/puppetlabs/bin/puppet module install maestrodev-wget
+#sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apt
 sudo /opt/puppetlabs/bin/puppet module install spantree/java8
 sudo /opt/puppetlabs/bin/puppet module install puppet-archive
-sudo /opt/puppetlabs/bin/puppet module install talend-sbt
-sudo /opt/puppetlabs/bin/puppet module install puppet-unattended_upgrades 
 
 echo "Setting up workstation... go grab some coffee :)"
 ./update.sh
