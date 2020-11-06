da50a7acfeb1b5dc1b0ad7c7f8bcd291b8a69e86
Alex Kuiper <alex@nightwhistler.net>
2020-11-06 18:56:06 +0100

    Added natural scrolling to mouse script

--- modules/i3/files/left_handed_mouse.sh
+++ modules/i3/files/left_handed_mouse.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 MOUSE_ID=`xinput list|grep -i mouse|grep id=|cut -f 2| cut -f 2 -d =`
+
+#Enable natural scrolling on external mouse
+#xinput set-prop $MOUSE_ID 276 1
+xinput set-prop $MOUSE_ID 312 1
+
+#Set left-handed buttons
 xinput set-button-map $MOUSE_ID 3 2 1
+
+
