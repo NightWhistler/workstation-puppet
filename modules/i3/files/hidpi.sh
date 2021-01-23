b930001c962d48675f1e931fe801c766b9cc606d
Alex Kuiper <alex@nightwhistler.net>
2021-01-23 13:46:59 +0100

    Added old DPI scripts back in

---
+++ modules/i3/files/hidpi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DPI=144
+echo $DPI > ~/.dpi
+
+xrandr --dpi $DPI
+xrandr --output eDP1 --scale 1x1
+sudo dconf write /org/gnome/desktop/interface/text-scaling-factor 1.5
---
+++ modules/i3/files/lowdpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 96 > ~/.dpi
+xrandr --dpi 96 
+#xrandr --output eDP1 --scale 0.66x0.66
+sudo dconf write /org/gnome/desktop/interface/text-scaling-factor 1.0
