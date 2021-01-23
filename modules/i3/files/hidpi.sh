b795bd3eea1b681a7d07cb35c5f750ced3bb2e30
Alex Kuiper <alex@nightwhistler.net>
2021-01-23 14:45:37 +0100

    Made DPI scripts also write to ~/.Xresources

--- modules/i3/files/hidpi.sh
+++ modules/i3/files/hidpi.sh
@@ -6,3 +6,5 @@ echo $DPI > ~/.dpi
 xrandr --dpi $DPI
 xrandr --output eDP1 --scale 1x1
 sudo dconf write /org/gnome/desktop/interface/text-scaling-factor 1.5
+
+echo "Xft.dpi: $DPI" > ~/.Xresources
--- modules/i3/files/lowdpi.sh
+++ modules/i3/files/lowdpi.sh
@@ -4,3 +4,5 @@ echo 96 > ~/.dpi
 xrandr --dpi 96 
 #xrandr --output eDP1 --scale 0.66x0.66
 sudo dconf write /org/gnome/desktop/interface/text-scaling-factor 1.0
+
+echo "Xft.dpi: $DPI" > ~/.Xresources
