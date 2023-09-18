83176540994b0e41817437bcd0c4eaf4b9264c1e
Alex Kuiper <alex@nightwhistler.net>
2023-09-18 18:11:44 +0200

    Updated regolith

--- modules/regolith/manifests/init.pp
+++ modules/regolith/manifests/init.pp
@@ -2,17 +2,17 @@ class regolith {
 
 
   apt::key { 'regolith_key':
-    id => 'FB9CEE656F304FFCC7A719F9BFE717649A5C3D08',
+    id => 'C91ECAB862037F9474087DBC7107DED133505B88',
     source => 'https://regolith-desktop.org/regolith.key'
   }
 
   apt::source { 'regolith':
-     location => 'https://regolith-desktop.org/release-ubuntu-jammy-amd64',
+     location => 'https://regolith-desktop.org/release-3_0-ubuntu-jammy-amd64 jammy main',
      repos    => 'main',
      require  => Apt::Key['regolith_key']
    }
 
-  package { 'regolith-desktop':
+  package { 'regolith-desktop', 'regolith-session-flashback', 'regolith-look-lascaille':
      ensure    => 'installed',
      require   => Apt::Source['regolith']
   }
